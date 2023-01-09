c3241ffc66223837db0d4f2c142085cc830c62b9
Piotr Kawa <piotr0589.pk@gmail.com>
2023-01-09 17:14:33 +0100

    Zabezpieczenie przed złośliwym użytkownikiem

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,11 +6,9 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        System.out.println("Ile danych chcesz wygenerować?");
 
-        int n = new Scanner(System.in).nextInt();
         IProgram program = new Program();
-        program.Start(n);
+        program.Start();
 
         System.out.println("Generacja danych zakończona!");
     }
