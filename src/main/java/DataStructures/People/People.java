2cbfe47089f77b902b5d554a046a5e90fe39006f
Piotr Kawa <piotr0589.pk@gmail.com>
2023-01-09 17:51:08 +0100

    Dodanie generatora numerów pesel

--- src/main/java/DataStructures/People/People.java
+++ src/main/java/DataStructures/People/People.java
@@ -1,9 +1,18 @@
 package DataStructures.People;
 
+import java.util.Date;
+
 public class People {
-    private int age;
+    private Date birthDate;
     private String name;
     private String username;
     private Gender gender;
+
+    public Gender GetGender() {
+        return null;
+    }
+    public Date GetBirthDate() {
+        return null;
+    }
 }
 
